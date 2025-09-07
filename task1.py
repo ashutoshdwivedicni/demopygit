0d0f1c9426d5070963ef1b1f0c72b19184cecbf1
ashutoshdwivedicni <ashutoshdwivedi.cni@gmail.com>
2025-09-07 23:18:04 +0530

    Update task1.py

--- task1.py
+++ task1.py
@@ -1,1 +1,12 @@
-
+a=input("Enter the first number:" )
+b=input("Enter the second number:" )
+a=int(a)
+b=int(b)
+c=a+b
+d=a-b
+e=a*b
+f=a/b
+print("addition:",c)
+print("subtraction:",d)
+print("multiplication:",e)
+print("division:",f)
