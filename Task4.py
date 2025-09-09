e21805fcbcc208bc4c5871d890301f8fb0171be0
ashutoshdwivedicni <ashutoshdwived.cni@gmail.com>
2025-09-09 11:15:38 +0530

    tested

--- Task4.py
+++ Task4.py
@@ -1,3 +1,4 @@
-for i in range(1,51):
-i=i+1
-print ("the sum of numbers1 to 50 is:",i)
+total_sum = 0 #sum stored in total_sum#
+for i in range(1, 51):
+    total_sum=total_sum+i
+print("The sum of integers from 1 to 50 is:", total_sum)
